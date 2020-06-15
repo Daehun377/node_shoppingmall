82c055f747d3d9a484ae14bdf7a0baffb20afc5f
Daehun Kim <gidonskaris@gmail.com>
2020-06-15 19:58:38 +0900

    My first node server

---
+++ server.js
@@ -0,0 +1,13 @@
+
+
+const express = require("express");
+const app = express(); // express 내에 있는 많은 함수들중에서 어떤 기능을 쓸것인지 . 으로 해서 불러오면 된다. why 가 아닌 how 로 접근 (개발을)
+
+
+
+
+
+
+const PORT = 3000;
+
+app.listen(PORT, console.log("server 시작됨"));
