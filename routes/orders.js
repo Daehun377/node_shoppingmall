7d368e49fcd4eed5631546c463675648d9637ad2
Daehun Kim <gidonskaris@gmail.com>
2020-06-15 21:07:53 +0900

    add order router

--- routes/orders.js
+++ routes/orders.js
@@ -1,3 +1,32 @@
+const express = require("express");
+const router = express.Router();
 
+//order create
+router.post("/", (req, res) => {
+    res.json({
+        message : "order create"
+    });
+});
 
+//order retrieve
+router.get("/", (req, res) => {
+    res.json({
+        message : "order retrieve"
+    });
+});
 
+//order update
+router.put("/", (req,res) => {
+    res.json({
+        message : "order update"
+    });
+});
+
+//order delete
+router.delete("/", (req,res) => {
+    res.json({
+        message : "order delete"
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,19 +3,19 @@
 const express = require("express");
 const app = express(); // express 내에 있는 많은 함수들중에서 어떤 기능을 쓸것인지 . 으로 해서 불러오면 된다. why 가 아닌 how 로 접근 (개발을)
 
-
-
-const productRoute = require("./routes/products");
-
 // app.use((req, res) => {
 //     res.json({
 //         message : "It works!"
 //     });
 // });
 
+const productRoute = require("./routes/products");
+
 app.use("/product", productRoute);
 
+const orderRoute = require("./routes/orders");
 
+app.use("/order", orderRoute);
 
 const PORT = 3000;
 
