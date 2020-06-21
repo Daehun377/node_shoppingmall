551b1a63a90441e06490c6618020621e8bc62781
Daehun Kim <gidonskaris@gmail.com>
2020-06-21 21:04:54 +0900

    Add order posting

--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const orderModel = require("../models/order");
 
 //order create
 router.post("/", (req, res) => {
-    res.json({
-        message : "order create"
+
+    const order = new orderModel({
+        product : req.body.productId,
+        quantity : req.body.qty
     });
+
+    order
+        .save()
+        .then(result => {
+            res.json({
+                message : "successful order saved",
+                orderInfo : {
+                    id: result._id,
+                    product : result.product,
+                    quantity : result.quantity,
+                    request : {
+                        type : "GET",
+                        url : "http://localhost:3000/order/" + result._id
+                    }
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                error : err.message
+            });
+        });
+
+    // res.json({
+    //     message : "order create"
+    // });
 });
 
 //order retrieve
