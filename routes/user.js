7c3f1ec29b14583272887b0b42d7ccd55001303a
Daehun Kim <gidonskaris@gmail.com>
2020-06-27 19:16:38 +0900

    Add user register

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,36 @@
 // 1
 const express = require("express");
 const router = express.Router();
+const userModel = require("../models/user");
 
 // 회원 가입
 
 router.post("/register", (req, res) => {
 
-})
+    const user = new userModel({
+        email : req.body.email,
+        password : req.body.password
+    })
+
+    user
+        .save()
+        .then(user => {
+            res.json({
+                message : "successful signup",
+                userInfo : {
+                    id : user._id,
+                    email : user.email,
+                    password : user.password
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                message : err.message
+            });
+        });
+
+});
 
 
 // 로그인
