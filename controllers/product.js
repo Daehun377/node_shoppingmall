9b8cafd3f11c8536fc433ea306867c11ec5c006f
Daehun Kim <gidonskaris@gmail.com>
2020-07-10 21:11:03 +0900

    Add product controller

---
+++ controllers/product.js
@@ -0,0 +1,176 @@
+
+
+
+const productModel = require("../models/product");
+
+
+
+
+exports.products_register_product = (req, res) => {
+
+    //모델의 새로운 instance 객체를 만들어 저장하는 법
+    const product = new productModel({
+        name : req.body.namelabel,
+        price : req.body.pricelabel
+    });
+
+
+
+    product
+        .save()
+        .then(result => {
+            res.json({
+                message : "successful saved",
+                productInfo : {
+                    id : result._id,
+                    name : result.name,
+                    price : result.price,
+                    request : {
+                        type : "GET",
+                        url : "http://localhost:3000/product/" + result._id
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            res.json({
+                error : err.message
+            });
+        });
+    //
+    //
+    // res.json({
+    //     message : "product create",
+    //     productInfo : product
+    // });
+};
+
+exports.products_get_all = (req, res) => {
+
+    productModel
+        .find()
+        .then(docs => {
+            const response = {
+                count : docs.length,
+                products : docs.map(doc => {
+                    return{
+                        id : doc._id,
+                        name : doc.name,
+                        price : doc.price,
+                        request : {
+                            type : "GET",
+                            url : "http://localhost:3000/product/" + doc._id
+                        }
+                    }
+                })
+            }
+            res.json(response)
+
+
+            // res.json({
+            //     message : "sucessful getData",
+            //     count : docs.length,
+            //     products : docs
+            // })
+        })
+        .catch(err => {
+            res.json({
+                error : err.message
+            });
+        });
+    // res.json({
+    //     message : "product retrieve"
+    // });
+};
+
+exports.products_get_detail = (req, res) => {
+
+    const id = req.params.productid;
+
+    productModel
+        .findById(id)
+        .then(doc => {
+            if(doc){
+                return res.status(200).json({
+                    message : "successful detail product",
+                    productInfo : {
+                        id : doc._id,
+                        name : doc.name,
+                        price : doc.price,
+                        request : {
+                            type : "GET",
+                            url :"http://localhost:3000/product"
+                        }
+                    }
+                });
+            }
+            // else{
+            //     res.status(404).json({
+            //         message : "product not found"
+            //     })
+            // }
+        })
+        .catch(err => {
+            res.json({
+                error : err.message
+            });
+        }); //밑에 있는게 더 우선시 됨으로, CATCH 의 메세지가 뜬다. catch는 무조건 있어야 함.
+
+};
+
+exports.products_update_product = (req,res) => {
+
+    const id = req.params.productid;
+
+    // update 할 내용 정의
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+
+
+    productModel
+        .updateOne({_id:id}, {$set:updateOps})
+        .then(() => {
+            res.status(200).json({
+                message : "updated product",
+                request : {
+                    type : "GET",
+                    url : "http://localhost:3000/product/" + id
+                }
+            });
+        })
+        .catch(err => {
+            res.json({
+                error : err.message
+            });
+        });
+    // res.json({
+    //     message : "product update"
+    // });
+};
+
+exports.products_delete_product = (req, res) =>{
+
+    const id = req.params.productid;
+
+    productModel
+        .deleteOne({_id:id})
+        .then(() => {
+            //console.log(result) 이렇게 console로 찍어보면 어떤 값이 나오는지 알 수 있음.
+            res.json({
+                message : "successful deleted product",
+                request :{
+                    type : "GET",
+                    url : "http://localhost:3000/product/"
+                }
+
+            });
+        })
+        .catch(err => {
+            res.json({
+                error : err.message
+            })
+        });
+
+};
--- routes/products.js
+++ routes/products.js
@@ -1,190 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const productModel = require("../models/product");
 const checkAuth = require("../config/check-auth");
 
+const {   //이건 직접 불러오는 거고, 만약 상수화 하면
+    products_get_all,
+    products_get_detail,
+    products_register_product,
+    products_update_product,
+    products_delete_product
+} = require("../controllers/product");
+
+// const productController = require("../controllers/product");
+//이렇게 해서 productContoller.products_register_products 이런식으로도 가능하지만,
+//이런 것을 줄이는 것이 좋은 코딩.
+
+
 // product create
-router.post("/" , checkAuth, (req, res) => {
-
-    //모델의 새로운 instance 객체를 만들어 저장하는 법
-    const product = new productModel({
-      name : req.body.namelabel,
-      price : req.body.pricelabel
-    });
-
-
-
-    product
-        .save()
-        .then(result => {
-            res.json({
-                message : "successful saved",
-                productInfo : {
-                    id : result._id,
-                    name : result.name,
-                    price : result.price,
-                    request : {
-                        type : "GET",
-                        url : "http://localhost:3000/product/" + result._id
-                    }
-                }
-            });
-        })
-        .catch(err => {
-            res.json({
-                error : err.message
-            });
-        });
-    //
-    //
-    // res.json({
-    //     message : "product create",
-    //     productInfo : product
-    // });
-});
-
-
-
+router.post("/" , checkAuth, products_register_product);
 
 //product total retrieve
-router.get("/", (req, res) => {
-
-    productModel
-        .find()
-        .then(docs => {
-            const response = {
-                count : docs.length,
-                products : docs.map(doc => {
-                    return{
-                        id : doc._id,
-                        name : doc.name,
-                        price : doc.price,
-                        request : {
-                            type : "GET",
-                            url : "http://localhost:3000/product/" + doc._id
-                        }
-                    }
-                })
-            }
-            res.json(response)
-
-
-            // res.json({
-            //     message : "sucessful getData",
-            //     count : docs.length,
-            //     products : docs
-            // })
-        })
-        .catch(err => {
-            res.json({
-                error : err.message
-            });
-        });
-    // res.json({
-    //     message : "product retrieve"
-    // });
-});
-
+router.get("/", products_get_all);
 
 //product detail get
-router.get("/:productid", checkAuth, (req, res) => {
-
-    const id = req.params.productid;
-
-    productModel
-        .findById(id)
-        .then(doc => {
-            if(doc){
-               return res.status(200).json({
-                    message : "successful detail product",
-                    productInfo : {
-                        id : doc._id,
-                        name : doc.name,
-                        price : doc.price,
-                        request : {
-                            type : "GET",
-                            url :"http://localhost:3000/product"
-                        }
-                    }
-                });
-            }
-            // else{
-            //     res.status(404).json({
-            //         message : "product not found"
-            //     })
-            // }
-        })
-        .catch(err => {
-            res.json({
-                error : err.message
-            });
-        }); //밑에 있는게 더 우선시 됨으로, CATCH 의 메세지가 뜬다. catch는 무조건 있어야 함.
-
-});
-
-
-
-
-
+router.get("/:productid", checkAuth, products_get_detail);
 
 //product update
-router.patch("/:productid", checkAuth, (req,res) => {
-
-    const id = req.params.productid;
-
-    // update 할 내용 정의
-    const updateOps = {};
-    for(const ops of req.body){
-        updateOps[ops.propName] = ops.value;
-    }
-
-
-    productModel
-        .updateOne({_id:id}, {$set:updateOps})
-        .then(() => {
-            res.status(200).json({
-                message : "updated product",
-                request : {
-                    type : "GET",
-                    url : "http://localhost:3000/product/" + id
-                }
-            });
-        })
-        .catch(err => {
-            res.json({
-                error : err.message
-            });
-        });
-    // res.json({
-    //     message : "product update"
-    // });
-});
+router.patch("/:productid", checkAuth, products_update_product);
 
 //product detail delete
-router.delete("/:productid", checkAuth, (req, res) =>{
+router.delete("/:productid", checkAuth,products_delete_product);
 
-    const id = req.params.productid;
 
-    productModel
-        .deleteOne({_id:id})
-        .then(() => {
-            //console.log(result) 이렇게 console로 찍어보면 어떤 값이 나오는지 알 수 있음.
-            res.json({
-                message : "successful deleted product",
-                request :{
-                    type : "GET",
-                    url : "http://localhost:3000/product/"
-                }
 
-            });
-        })
-        .catch(err => {
-            res.json({
-                error : err.message
-            })
-        });
 
-});
+
 
 
 module.exports = router;
